fb2b498a85bce659df7463373a71207d22f8b859
Nikola <nidza.bg@gmail.com>
2021-04-29 10:55:56 +0200

    Created ProductController.php

---
+++ App/Http/Controllers/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/*
+ *  IndexController will take the role of indexing 3x3 products, as per project request
+ *  and current routing methods.
+ */
+
+class ProductController {
+
+}
