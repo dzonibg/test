9194a566ba10474bcbcaa2263d06ea33645bf84c
Nikola <nidza.bg@gmail.com>
2021-04-29 14:48:57 +0200

    Indexing all products on index page. Todo add limits and pagination.

--- App/Http/Controllers/IndexController.php
+++ App/Http/Controllers/IndexController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
+use App\Models\Comment;
 
 class IndexController {
 
     public function index() {
-        return view("index/index");
+        $products = new Product();
+        $products = $products->fetchAll();
+
+        return view("index/index", compact("products"));
     }
 
     public function show() {
--- Views/index/index.view.php
+++ Views/index/index.view.php
@@ -1,1 +1,20 @@
-We're a Citrus selling company. We're growing fast.
+<div class="container">
+
+    <p>We're a Citrus selling company. Growing fast. Take our lemon, make a lemonade.</p>
+
+    <div class="row">
+    <?php foreach ($products as $product) { ?>
+                <div class="col-sm-4">
+                     <div class="card">
+                        <div class="card-header"><?=$product->title?></div>
+                         <div class="card-body">
+                             <img class="img-fluid" src="/storage/product/images/<?=$product->image?>">
+                             <p><?=$product->description?></p>
+                         </div>
+                     </div>
+                 </div>
+
+    <?php } ?>
+    </div>
+
+</div>
